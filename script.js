27d14795f934a0258e75952211e047e4a9004bff
Rohit Umbare <127503827+rohitumbare@users.noreply.github.com>
2024-10-27 09:42:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// nav animate
-
-// stagger one by one
-
-// gsap.from(".nav h3", {
-//   y: -50,
-//   opacity: 0,
-//   duration: 1,
-//   delay: 0.5,
-//   stagger: 1,
-// });
-
-// gsap.from(".main h1", {
-//   y: -50,
-//   opacity: 0,
-//   duration: 2,
-//   delay: 2,
-//   stagger: 1,
-// });
 
 var timeline = gsap.timeline();
 
